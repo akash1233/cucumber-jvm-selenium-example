079c1ace9194b27f8b8c32f1eaac9c5f512e627f
dharmendrasingh@msn.com <Hr32sapient>
2015-08-20 19:58:01 -0400

    Added Saucelabs support .

--- src/test/java/com/michalvich/cucumber/selenium/example/GoogleSearchScenario.java
+++ src/test/java/com/michalvich/cucumber/selenium/example/GoogleSearchScenario.java
@@ -6,37 +6,42 @@ import cucumber.annotation.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.net.URL;
+
 import static org.junit.Assert.assertTrue;
 
 public class GoogleSearchScenario {
     private RemoteWebDriver driver;
 
-	@Given("^the page is open \"([^\"]*)\"$")
-	public void the_page_is_open(String page) throws Throwable {
-		DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
-		caps.setCapability("platform", "Windows 10");
-		caps.setCapability("version", "11.0");
-		caps.setJavascriptEnabled(true);
-		driver = new RemoteWebDriver(new URL("http://D_Singh:44c07910-d4a8-4430-8ec6-e31012792bab@ondemand.saucelabs.com:80/wd/hub"), caps);
-		driver.get(page);
-	}
+    @Given("^the page is open \"([^\"]*)\"$")
+    public void the_page_is_open(String page) throws Throwable {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setBrowserName(System.getenv("SELENIUM_BROWSER"));
+        desiredCapabilities.setVersion(System.getenv("SELENIUM_VERSION"));
+        desiredCapabilities.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
+        driver = new RemoteWebDriver(
+                new URL("http://D_Singh:44c07910-d4a8-4430-8ec6-e31012792bab@ondemand.saucelabs.com:80/wd/hub"),
+                desiredCapabilities);
+        driver.get(page);
+    }
 
-	@When("^I search for \"([^\"]*)\"$")
-	public void I_search_for(String search) throws Throwable {
-		WebElement element = driver.findElement(By.name("q"));
-		element.sendKeys(search);
-		element.submit();
-	}
+    @When("^I search for \"([^\"]*)\"$")
+    public void I_search_for(String search) throws Throwable {
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys(search);
+        element.submit();
+    }
 
-	@Then("^a browser title should contains \"([^\"]*)\"$")
-	public void a_browser_title_should_contains(String text) throws Throwable {
-        System.out.println("----------------Title contains " + driver.getTitle()+ "----------------------");
-		assertTrue(driver.getTitle().contains(text));
-		driver.close();
-		driver.quit();
-	}
+    @Then("^a browser title should contains \"([^\"]*)\"$")
+    public void a_browser_title_should_contains(String text) throws Throwable {
+        System.out.println("----------------Title contains " + driver.getTitle() + "----------------------");
+        assertTrue(driver.getTitle().contains(text));
+        driver.close();
+        driver.quit();
+    }
 
 }

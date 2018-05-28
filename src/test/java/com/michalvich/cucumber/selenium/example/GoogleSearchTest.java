836cc4a8a464115595514a498912dd21287eb27e
dharmendra singh <dharmendrasingh@msn.com>
2018-05-28 19:17:37 -0400

    Fixed the saucelabs paath.

--- src/test/java/com/michalvich/cucumber/selenium/example/GoogleSearchTest.java
+++ src/test/java/com/michalvich/cucumber/selenium/example/GoogleSearchTest.java
@@ -6,6 +6,5 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @Cucumber.Options(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class GoogleSearchTest {
-	
-	
+
 }
